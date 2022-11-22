5b3d6892726ac3c37bf5e9905ca349f95f21465c
Cozmu <www.jorgereis@gmail.com>
2022-11-22 17:00:05 -0300

    Adicionando requisito 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import './App.css';
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+// import useFetch from '../hooks/useFetch';
+import Table from '../components/Table';
 
 function Home() {
+  const [planetsAPI, setPlanetAPI] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setErrror] = useState(null);
+  // const { data, error, isLoading } = useFetch('https://swapi.dev/api/planets');
+
+  const toSeek = async () => {
+    try {
+      const endPoint = 'https://swapi.dev/api/planets';
+      const response = await fetch(endPoint);
+      const data = await response.json();
+      const filter = data.results.filter((e) => delete e.residents);
+      setPlanetAPI(filter);
+    } catch (e) {
+      setErrror(e);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    toSeek();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Carregando...</h1>
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div>
+        <p>{error}</p>
+      </div>
+    );
+  }
+
   return (
-    <div>Home</div>
+    <div>
+      <Table
+        planetsAPI={ planetsAPI }
+      />
+    </div>
   );
 }
 

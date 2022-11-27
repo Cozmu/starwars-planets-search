aac40e4c503d925b70cb1f939889772c23dd39ec
Cozmu <www.jorgereis@gmail.com>
2022-11-27 14:23:11 -0300

    Adicionando estilização (filtros)

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,23 +1,28 @@
 import { useContext } from 'react';
+import { RiDeleteBin7Fill } from 'react-icons/ri';
 import StarWarsContext from '../context/StarWarsContext';
+import '../style/Filters.css';
 
 function Filters() {
   const { filtersCollection, remuveFilter } = useContext(StarWarsContext);
   return (
-    <div>
+    <div className="filters-container">
       {filtersCollection.filterByNumericValues
         ?.map(({ valueFilter, comparisonFilter, columnFilter }, i) => (
           <section
             key={ i }
             data-testid="filter"
-            className="filtro"
+            className="filters"
           >
-            <p>{`${columnFilter} ${comparisonFilter} ${valueFilter}`}</p>
+            <p>
+              {`${columnFilter} ${comparisonFilter} ${valueFilter} `}
+            </p>
             <button
               type="button"
+              className="removed-filters"
               onClick={ () => remuveFilter(columnFilter) }
             >
-              Remover
+              <RiDeleteBin7Fill className="removed-icon" />
             </button>
           </section>
         ))}

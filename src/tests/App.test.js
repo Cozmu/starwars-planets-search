84d07ae680718a364fcec0e93136f79868d89b71
Cozmu <www.jorgereis@gmail.com>
2022-11-29 13:58:25 -0300

    test nao funcionando

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,29 +1,37 @@
 import React from 'react';
-import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import { render, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
 import App from '../App';
 import planetsAPI from './helpers/MockPlanets'
 import MockFetch from './helpers/MockFetch'
 
+afterEach(() => jest.clearAllMocks()); 
+
 describe('Form coverage' , () => {
   it('Verifique se os inputs são renderizados corretamente' , async () => {
-    global.fetch = jest.fn(() => Promise.resolve({
-      json: () => Promise.resolve(planetsAPI),
-    }));
+    // global.fetch = jest.fn(() => Promise.resolve({
+    //   json: () => Promise.resolve(planetsAPI),
+    // }));
+    global.fetch = jest.fn(async () => ({
+      json: async () => planetsAPI
+    })); 
     render(<App />);
-    await waitForElementToBeRemoved(() =>
-    screen.getByText(/Carregando.../i),
-    )
+     await waitForElementToBeRemoved(() =>
+      screen.queryByText(/Carregando.../i),
+     )
+    await waitFor(async () => {
+      expect(screen.queryByText('Tatooine')).toBeInTheDocument();
+    }, { timeout: 3000 }) 
     
-    expect( await screen.findByTestId('name-filter')).toBeInTheDocument();
-    expect(screen.getByTestId('column-filter')).toBeInTheDocument();
-    expect(screen.getByTestId('comparison-filter')).toBeInTheDocument();
-    expect(screen.getByTestId('value-filter')).toBeInTheDocument();
-    expect(screen.getByTestId('button-filter')).toBeInTheDocument();
-    expect(screen.getByTestId('column-sort')).toBeInTheDocument();
-    expect(screen.getByTestId('column-sort-input-asc')).toBeInTheDocument();
-    expect(screen.getByTestId('column-sort-input-desc')).toBeInTheDocument();
-    expect(screen.getByTestId('column-sort')).toBeInTheDocument();
-    expect(screen.getByTestId('column-sort-button')).toBeInTheDocument();
+    // expect( await screen.findByTestId('name-filter')).toBeInTheDocument();
+    // expect(screen.getByTestId('column-filter')).toBeInTheDocument();
+    // expect(screen.getByTestId('comparison-filter')).toBeInTheDocument();
+    // expect(screen.getByTestId('value-filter')).toBeInTheDocument();
+    // expect(screen.getByTestId('button-filter')).toBeInTheDocument();
+    // expect(screen.getByTestId('column-sort')).toBeInTheDocument();
+    // expect(screen.getByTestId('column-sort-input-asc')).toBeInTheDocument();
+    // expect(screen.getByTestId('column-sort-input-desc')).toBeInTheDocument();
+    // expect(screen.getByTestId('column-sort')).toBeInTheDocument();
+    // expect(screen.getByTestId('column-sort-button')).toBeInTheDocument();
   })
  
 })
